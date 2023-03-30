9e5c8ba9e002c51a8a18925d48510c8b8a9c404a
Timilehin Olusa <olusatimmie07@gmail.com>
2023-03-30 14:20:41 -0700

    Debuged

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
-	newNode = malloc sizeof(binary_tree_t);
+	newNode = malloc(sizeof(binary_tree_t));
 	if (newNode == NULL)
 	{
 		return (NULL);
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		newNode->parent = parent;
 		newNode->n = value;
 		newNode->left = NULL;
-		newNode->Right = NULL;
+		newNode->right = NULL;
 	}
 	return (newNode);
 }
