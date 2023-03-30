c67d8b9ebfd315dd784eae0eb072437f6101e364
Timilehin Olusa <olusatimmie07@gmail.com>
2023-03-30 14:59:50 -0700

    Completed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,6 +27,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		{
 			binary_tree_t *temp;
 
+			temp = malloc(sizeof(binary_tree_t));
 			if (temp == NULL)
 			{
 				return (NULL);
@@ -34,6 +35,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 			temp = parent->left;
 			parent->left = newNode;
 			newNode->left = temp;
+			free(temp);
 		}
 		newNode->n = value;
 		newNode->right = NULL;
